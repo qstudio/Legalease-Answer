be5dbbe049a671bc3a0f046b138e6aed9968da9c
Ryan Syntax <ryansyntax@gmail.com>
2021-04-27 17:10:00 +0100

    Update legalese-answer.php
    function descs, input validation rules, rest connected

--- legalese-answer.php
+++ legalese-answer.php
@@ -5,7 +5,7 @@
  * Description: Legalese Answer Plugin for Legalease.com
  * Author: Ryan Syntax
  * Author URI: https://ryansyntax.com
- * Version: 1.0
+ * Version: 1.1
  * Text Domain: legalese-answer
  */
  
@@ -19,7 +19,17 @@
 		 // init main
     	add_action('init', array($this, 'main'));
     }
-    
+    /**
+	 * main
+	 *
+	 * Pulls core functions into use
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	void
+	 * @return	void
+	 */
     public function main() {
 		// Check post data
 		$this->post_data_check();
@@ -30,36 +40,65 @@
 		// Adds new fields to profile page
 		$this->profile_additions();
 
-		// Rest Calls (WIP)
-		//$this->rest_api();
+		// Pull in rest calls
+		$this->rest_api();
     } 
 
-	// Check if post data
+	/**
+	 * post_data_check
+	 *
+	 * Checks the post data for inputs
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	void
+	 * @return	void
+	 */
 	public function post_data_check() {
 		if (is_user_logged_in()) {
 			if (isset($_POST['legalese_answer_value'])) {
 				$le_answer = (string) $_POST['legalese_answer_value'];
-				if ($this->validation($le_answer)) {
+				// Validation rules
+				$val_args = ['maxlen' => 40];
+				
+				if ($this->validation($val_args, $le_answer)) {
 					$this->update_answer_field( get_current_user_id(), $le_answer);
 				}
 			}
 		}
 	}
 
-	// Variable validation checks
-	public function validation($args) {
+	/**
+	 * validation
+	 *
+	 * Checks variables are using correct characters
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	array $args The array of validation arguments to pass
+	 * @return	bool
+	 */
+	public function validation($args, $value) {
 		// Max character length
 		if (isset($args['maxlen'])) {
-			if ( strlen($args['value']) > $args['maxlen']) {
+			if ( strlen($value) > $args['maxlen']) {
 				$fail_count++;
 			}
 		}
 		// Regex categories
 		if (isset($args['chars'])) {
-			if ($args['chars'] == 'num') {
-
-			} elseif ($args['chars'] == 'letter') {
-
+			if ($args['chars'] == 'number') {
+				// Checks if value is numeric
+				if (!is_numeric($value)) {
+					$fail_count++;
+				}
+			} elseif ($args['chars'] == 'alphabet') {
+				// Checks if value is alphabetical
+				if (!ctype_alpha($value)) {
+					$fail_count++;
+				}
 			}
 		}
 
@@ -70,7 +109,17 @@
 		}
 	}
 
-	// Setup virtual page
+	/**
+	 * virtual_page
+	 *
+	 * Creates path to virtual page legalese-answer
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	void
+	 * @return	void
+	 */
 	public function virtual_page() {
 		// Rewrite Path
 		add_rewrite_rule('^legalese-answer/?', 'index.php?legalese-answer=virtual_page', 'top');
@@ -81,21 +130,59 @@
 			return $query_vars;
 		});
 		// Redirect to template
-		add_action('template_redirect', function(){
+		add_action('template_redirect', 'virtual_legalese_answer_page');
+		
+		/**
+	 	* virtual_legalese_answer_page
+	 	*
+	 	* Displays legalese-answer template page when criteria is met
+	 	*
+	 	* @date	27/4/21
+	 	* @since	1.1
+	 	*
+	 	* @param	void
+	 	* @return	void
+	 	*/
+		function virtual_legalese_answer_page(){
 			$custom =  get_query_var('legalese-answer');
 			if ( $custom ) {
 				include plugin_dir_path( __FILE__ ) . 'templates/legalese-answer-page.php';
 				die;
 			}
-		});
+		}
 	}
 
-	// Profile page adaptations
+	/**
+	 * profile_additions
+	 *
+	 * Adds additional values to admin/profile page
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	void
+	 * @return	void
+	 */
 	public function profile_additions() {
 		// Show user profile page
-		add_action( 'show_user_profile', function() {
-			$meta = get_user_meta( get_current_user_id() );
-			echo 'show profile'; ?>
+		add_action( 'show_user_profile', 'legalese_answer_field' );
+
+		// Edit user profile page
+		add_action( 'edit_user_profile', 'legalese_answer_field');
+
+		/**
+	 	* legalese_answer_field
+	 	*
+	 	* Displays html form table for legalese_answer
+	 	*
+	 	* @date	27/4/21
+	 	* @since	1.1
+		*
+	 	* @param	void
+	 	* @return	void
+	 	*/
+		function legalese_answer_field() {
+			$meta = get_user_meta( get_current_user_id() ); ?>
 			<table class="form-table" role="presentation">
 				<tbody>
 					<tr class="user-url-wrap">
@@ -105,16 +192,24 @@
 				</tbody>
 			</table>
 			<?php 
-		} );
-		// Edit user profile page
-		add_action( 'edit_user_profile', function() {
-			echo 'edit profile hook';
-		} );
+		}
+		
 
 		// Allow save of additional field
 		add_action( 'personal_options_update', 'save_extra_user_profile_field' );
 		add_action( 'edit_user_profile_update', 'save_extra_user_profile_field' );
-		// Save answer field behaviour
+
+		/**
+	 	* save_extra_user_profile_field
+	 	*
+	 	* Saves the additional profile field on edit success
+	 	*
+	 	* @date	27/4/21
+	 	* @since	1.1
+		 *
+	 	* @param	$user_id
+	 	* @return	void
+	 	*/
 		function save_extra_user_profile_field( $user_id ) {
 
 			if ( !current_user_can( 'edit_user', $user_id ) ) { 
@@ -125,42 +220,83 @@
 		}
 
 	}
-	// Rest catch
+	/**
+	 * rest_api
+	 *
+	 * Deals with Rest API Requests
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	void
+	 * @return	void
+	 */
 	public function rest_api() {
 		// Preparing to serve a REST API request
 		add_action( 'rest_api_init', 'adding_user_meta');
 
+		/**
+	 	* adding_user_meta
+	 	*
+	 	* Adds user meta 
+	 	*
+	 	* @date	27/4/21
+	 	* @since	1.1
+	 	*
+	 	* @param	void
+	 	* @return	void
+	 	*/
 		function adding_user_meta() {
 			register_rest_field( 'user',
 								 'collapsed_widgets',
 								  array(
-									'get_callback'      => 'user_meta_callback',
+									'get_callback'      => 'user_meta_call',
 									'update_callback'   => null,
 									'schema'            => null,
 									 )
 							   );
-		 }
-
-		// Callback of Meta Value
+		}
+		/**
+	 	* user_meta_call
+	 	*
+	 	* Callback of meta value
+	 	*
+	 	* @date	27/4/21
+	 	* @since	1.1
+	 	*
+	 	* @param	string $user
+		* @param	string $field_name
+		* @param	string $request
+	 	* @return	string
+	 	*/
 		function user_meta_call( $user, $field_name, $request) {
 			return get_user_meta( $user[ 'id' ], $field_name, true );
 		}
 	}
 
 
- 	// Update new value to legalese answer
+ 	/**
+	 * update_answer_field
+	 *
+	 * Updates the answer value for set user
+	 *
+	 * @date	27/4/21
+	 * @since	1.1
+	 *
+	 * @param	int $user_id The user ID
+	 * @param	string $value The answer value
+	 * @return	void
+	 */
 	 public function update_answer_field($user_id, $value) {
-		// Typecast Inputs
+		// Typecast inputs
 		$user_id = (int) $user_id;
 		$post_id = (string) 'user_' . $user_id;
 		$value = (string) $value;
 	
-		// Update Field using ACF Function
+		// Update field using ACF function
 		update_field('legalese_answer', $value, $post_id);
 	}
 }
 
-// Load class once
-//if( !class_exists('LegaleseAnswer') ) {
-	new LegaleseAnswer;
-//}
+// Load Class
+new LegaleseAnswer;
